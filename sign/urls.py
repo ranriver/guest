47aab215bd6f12ca10b607f9f979bd3cda276c73
ranriver <39619258+ranriver@users.noreply.github.com>
2022-12-13 11:37:15 +0800

    Add files via upload

---
+++ sign/urls.py
@@ -0,0 +1,27 @@
+# _*_ coding: utf-8 _*_
+# @Time: 2022/12/9 19:33
+# @Author: river
+# @File: urls.py
+from django.urls import path, include
+from sign import views_if
+
+urlpatterns = [
+    # sign system interface:
+    # ex : /api/add_event/
+    path('add_event/', views_if.add_event, name='add_event'),
+    # ex : /api/add_guest/
+    path('add_guest/', views_if.add_guest, name='add_guest'),
+    # ex : /api/get_event_list/
+    path('get_event_list/', views_if.get_event_list, name='get_event_list'),
+    # ex : /api/get_guest_list/
+    path('get_guest_list/', views_if.get_guest_list, name='get_guest_list'),
+    # ex : /api/user_sign/
+    path('user_sign/', views_if.user_sign, name='user_sign'),
+    # security interface:
+    # ex : /api/sec_get_event_list/
+    # path('sec_get_event_list/', views_if_sec.get_event_list, name='get_event_list'),
+    # ex : /api/sec_add_event/
+    # path('sec_add_event/', views_if_sec.add_event, name='add_event'),
+    # ex : /api/sec_get_guest_list/
+    # path('sec_get_guest_list/', views_if_sec.get_guest_list, name='get_guest_list'),
+]
