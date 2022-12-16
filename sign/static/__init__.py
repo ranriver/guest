d0c3d5106629574c5245ef562620ef29a6311e7e
ranriver <39619258+ranriver@users.noreply.github.com>
2022-12-16 17:41:21 +0800

    添加静态文件

---
+++ sign/static/__init__.py
@@ -0,0 +1,4 @@
+# _*_ coding: utf-8 _*_
+# @Time: 2022/12/15 18:48
+# @Author: river
+# @File: __init__.py.py
---
+++ sign/static/image/__init__.py
@@ -0,0 +1,4 @@
+# _*_ coding: utf-8 _*_
+# @Time: 2022/12/15 18:58
+# @Author: river
+# @File: __init__.py.py
